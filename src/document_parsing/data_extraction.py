5527de200662a60cecfb3a512b01533591d43f31
Mamoon018 <mamonhaider11@gmail.com>
2025-10-25 20:02:10 +0200

    Accessed the parsed json output file of MinerU

--- src/document_parsing/data_extraction.py
+++ src/document_parsing/data_extraction.py
@@ -3,7 +3,7 @@
 ## Let's create the rough subprocess and test it 
 import subprocess
 from subprocess import CalledProcessError, TimeoutExpired
-import shlex
+from pathlib import Path
 
 ## Parsing Module Workflow
 """
@@ -11,8 +11,8 @@ import shlex
 Class MinerU_Parser:
 
     1- Parse_user_document
-    2- Run_minerU
-    3- Read_minerU_output
+    2- Run_minerU (Done)
+    3- Read_minerU_output (In-progress)
     4- Format_the_minerU_output
     5- Run_LibreOffice
     6- Check_installations
@@ -42,7 +42,7 @@ class MinerU_Parser():
         data_file_path (path): It is the file path of the user input file that minerU will parse.
 
         **Returns:**
-        output_file_path (path): It is the path of the folder that minerU returns which contains all sort of parsed data, JSON, Markdown etc.
+        subprocess_minerU_output (path): It is the path of the folder that minerU returns which contains all sort of parsed data, JSON, Markdown etc.
 
         **Raises:**
         It raises either CalledProcessError or TimeoutExpiredError if external programs gives error or gets hang & block indifinitely. 
@@ -59,14 +59,14 @@ class MinerU_Parser():
             # lets define the subprocess for minerU
             data_file_path = self.data_file_path
 
-            subprocess_minerU_output = subprocess.run([r"C:\\Users\\Hp\\AppData\\Local\\Programs\\MinerU\\MinerU.exe", "--path", f"{data_file_path}", "--output", "C:\\Users\\Hp\\Documents\\AI Projects docs\\RAG\\Parsed_output", 
+            subprocess_minerU_output = subprocess.run(["C:\\Users\\Hp\\AppData\\Local\\Programs\\MinerU\\MinerU.exe", "--path", f"{data_file_path}", "--output", "C:\\Users\\Hp\\Documents\\AI Projects docs\\RAG\\Parsed_output", 
                                             "--method", "auto", 
                                             "--backend", "pipeline", 
                                             "--start", "0", 
                                             "--end" "2"],
                                             check=True,
                                             capture_output= True
-                                            )
+                                            ,cwd="C:\\Users\\Hp\\Documents\\AI Projects docs\\RAG" )
             
             MinerU_error = subprocess_minerU_output.returncode
             MinerU_output = subprocess_minerU_output.stdout.decode("utf-8")
@@ -82,16 +82,45 @@ class MinerU_Parser():
                 raise("Subprocess got hanged or blocked indefinitely - here is the timeoutexpired error {e}") 
             
 
+    # lets create the method to read the output file of the minerU output
+    def read_minerU_output(self):
+        """
+        It takes the address of the folder where minerU has stored its output and will access json file
+        from it. It is upto us to use whichever we want to out of these two output files.
 
+        **Args:**
+        minerU_output_path (path): It is the path of the folder where minerU has stored its parsed output files.
 
+        **Returns:**
+        Parsed_JSON_file : It is the parsed data in json format.
 
+        """
+    ## Let's explore the Pathlib library
 
+        # set directory
+        new_cwd_path = Path("C:\\Users\\Hp\\Documents\\AI Projects docs\\RAG")
+        # How to find the file using extension & then read those files & then save them in another folder.
+        for files in new_cwd_path.glob("*.json"):
+            list_of_json_files = [files.name][0]
 
+        # create folder in the same directory
+        parsed_output_path = new_cwd_path.joinpath("parsed_output","layout.json")
+        # read the json file in the RAG folder
+        dummy_parsed_json_path = new_cwd_path.joinpath(f"{list_of_json_files}")
+        content_json_file = dummy_parsed_json_path.read_text(encoding="utf-8")
 
+        # move file to folder parsed_output
+        with parsed_output_path.open(mode='wb') as file:
+            file.write(dummy_parsed_json_path.read_bytes())
 
+        return content_json_file
 
 
 
 parse_data = MinerU_Parser(data_file_path="C:\\Users\Hp\\Documents\\AI Projects docs\\RAG\\RAG_for_Anything.pdf")
+
+# MinerU parsing output
 minerU_output = parse_data.run_minerU()
-print(minerU_output)
+# MinerU json file
+minerU_json_file = parse_data.read_minerU_output()
+print(minerU_json_file)
