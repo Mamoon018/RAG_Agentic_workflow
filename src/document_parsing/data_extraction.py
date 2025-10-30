26cf7ead44fe650cf97781867408b230c1c3d74f
Mamoon018 <mamonhaider11@gmail.com>
2025-10-30 20:22:18 +0100

    Added installations check, completed knowledge unit method, tested data parsing module successfully

--- src/document_parsing/data_extraction.py
+++ src/document_parsing/data_extraction.py
@@ -5,6 +5,7 @@ import subprocess
 from subprocess import CalledProcessError, TimeoutExpired
 from pathlib import Path
 import time
+import json
 
 ## Parsing Module Workflow
 """
@@ -14,8 +15,7 @@ Class MinerU_Parser:
     1- Parse_user_document
     2- Run_minerU (Done)
     3- Read_minerU_output (Done)
-    4- Format_the_minerU_output 
-    5- Run_LibreOffice
+    4- Format_the_minerU_output (Done)
     6- Check_installations
 
 """
@@ -120,42 +120,147 @@ class MinerU_Parser():
         json_output_path = (Path(output_dir)).joinpath("layout.json")
         md_output_path = (Path(output_dir)).joinpath("full.md")
         
-        self.content_of_json =  json_output_path.read_text(encoding="utf-8")
+        content_of_json =  json_output_path.read_text(encoding="utf-8")
+        # let's convert this raw text into JSON
+        self.content_of_json = json.loads(content_of_json)
         self.content_of_md = md_output_path.read_text(encoding="utf-8")
               
          
-        return self.content_of_md, self.content_of_json
+        return self.content_of_json
 
 
-    # Lets create the method to format the output of the minerU
-
+        # Lets create the method to format the output of the minerU & get the knowledge units for Text & Tables
     def format_minerU_output(self):
 
-        """
-        It takes the JSON output of the minerU, and fetches the information elements required to create the knowledge units. 
+            """
+            It takes the JSON output of the minerU, and fetches the information elements required to create the knowledge units. 
 
-        **Args:**
-        self.content_of_json (markdown): It is the parsed output in the JSON format.
+            **Args:**
+            self.content_of_json (markdown): It is the parsed output in the JSON format.
+            ABS_PATH (Path): It is the absolute path that we need to use in order to convert the relative path of the table_image to the
+            absolute path. For now, we do not have the output of minerU path and our input path same (Because minerU is not running command statement correctly)
+            so, we are manually bringing the path of the minerU output manually. Otherwise, we would just have to import it using path.cwd().
+            
+            **Returns:**
+            self.content_list (list): It is the list of the knowledge units that contains the information about part of the document.
+
+            **Raises:**
+
+            """
+            # lets get the input of the function
+            minerU_raw_output = self.content_of_json
+            ABS_PATH = Path("C:\\Users\\Hp\\MinerU")
+
+
+            PDF_INFO = minerU_raw_output.get("pdf_info","")
+            content_list = []
+            table_images_folder = []
+
+
+            # Let's get the directory path of the output of the minerU so, that we can create the absolute path of the table images. 
+            for items in ABS_PATH.iterdir():
+                if items.is_dir():
+                    table_images_folder.append(items)
+            ABS_PATH_OUTPUT = table_images_folder[1]
+            
+
+            # Lets get all the knowledge units in the content list
+            for page in PDF_INFO:
+                page_no = page.get("page_idx","")
+                discarded_block = page.get("discarded_blocks","")
+                para_blocks = page.get("para_blocks","")
+                for para_dict_details in para_blocks:
+                    para_label = para_dict_details.get("type","")
+                    if para_label not in ["table", "text", "title"]:
+                        continue
+                    
+                    # Knowledge units of textual content 
+                    if para_label in ["text", "title"]:
+                        para_lines = para_dict_details.get("lines","")
+                        for lines_dict_details in para_lines:
+                            spans_details = lines_dict_details.get("spans","")
+                            for span_dict_details in spans_details:
+                                    content_of_span = span_dict_details.get("content","")
+                                    content_type_of_span = span_dict_details.get("type","")
+                                    if content_type_of_span not in ["text"]:
+                                        continue
+                                    knowledge_unit = {"page_no.":page_no, "raw_content":content_of_span, "content type": para_label}
+                                    content_list.append(knowledge_unit)
+
+                    # Knowledge units of tabular content
+                    elif para_label == "table":
+                        table_blocks = para_dict_details.get("blocks","")
+                        for table_block in table_blocks:
+                            table_block_lines = table_block.get("lines","")
+                            for lines_dict_details in table_block_lines:
+                                spans_details = lines_dict_details.get("spans","")
+                                for table_span_dict_details in spans_details:
+                                        table_of_block_line = table_span_dict_details.get("image_path","")
+                                        content_type_of_span = table_span_dict_details.get("type","")
+                                        table_caption = table_span_dict_details.get("content","")
+                                        # Lets convert the relative path of the table image to the absolute path
+
+                                        ABS_table_image_path = ABS_PATH_OUTPUT.joinpath(f"images/{table_of_block_line}")
+                                        if content_type_of_span not in  ["table", "text"]:
+                                            continue
+                                        if content_type_of_span == "table":
+                                            knowledge_unit = {"page_no.":page_no, "table_image_path":ABS_table_image_path, "content_type": para_label}
+                                        if content_type_of_span == "text": 
+                                            knowledge_unit = {"page_no.":page_no, "table_caption":table_caption, "content_type": para_label}
+
+                                        content_list.append(knowledge_unit)
+                    
+
+
+            return content_list
+
+    # Lets create the method to check the installations
+    def check_minerU_installation(self):
+        """
+        This method checks the installation of the required tools to run the parser module. For parsing the document,
+        we need to have minerU installed in the system and therefore, it is important to check if the user has installed
+        it correctly or not. This is more like check function to ensure that the pre-requisites of running the program are
+        completed or not.        
         
-        **Returns:**
-        self.content_list (list): It is the list of the knowledge units that contains the information about part of the document.
-
-        **Raises:**
-
-        Features of content list: Content_list(content_data, content_type, content_metadata)
-        STEPS TO CREATE A CONTENT LIST:
-        1- PDF info contains the dict, which contains multiple "Paras" as strings of dictionaries.
-        2- Each "Para" contains the LIST of Dictionaries as value.
-        3- In the list, each dictionary contains "lines" as string.
-        4- lines contain the LIST of Dictionary as value.
-        5- Dictionary contains "Span" as string, and LIST of Dictionary as value.
-        6- Dictionary contains the "type" and "content" as string & their values represent the data type & raw content.
-
         """
-        # lets get the input of the function
-        minerU_raw_output = self.content_of_json
+        try:
+
+            minerU_subprocess_kwargs = {
+                "capture_output": True,
+                "text": True,
+                "encoding": "utf-8",
+                "check": True
+            }
+
+            
+            # let's ensure that while checking, console window on Windows remains hidden
+            # import platform to check the system
+            import platform
+            if platform.system == "Windows":
+                minerU_subprocess_kwargs["creationflags"] = subprocess.CREATE_NO_WINDOW
+
+            run_minerU_for_checking = subprocess.run(["C:\\Users\\Hp\\AppData\\Local\\Programs\\MinerU\\MinerU.exe", "--version"], **minerU_subprocess_kwargs)
+
+            return True
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            return False
 
 
+# Lets run the module with the main function
 
-        return PDF_INFO
+def main():
 
+    """
+    This function takes the file from the user in order to parse it,
+    and also gives us its knowledge units. 
+    
+    """
+    minerU_testing = MinerU_Parser(data_file_path="C:\\Users\Hp\\Documents\\AI Projects docs\\RAG\\RAG_for_Anything.pdf")
+
+    run_minerU = minerU_testing.run_minerU()
+    read_minerU = minerU_testing.read_minerU_output()
+    format_minerU = minerU_testing.format_minerU_output()
+
+    print(format_minerU)
+
+main()
--- src/document_parsing/test.py
+++ src/document_parsing/test.py
@@ -13,93 +13,9 @@ minerU_testing = MinerU_Parser(data_file_path="C:\\Users\Hp\\Documents\\AI Proje
 #minerU_formatted_output = minerU_testing.format_minerU_output()
 #print(minerU_formatted_output)
 
+run_minerU = minerU_testing.run_minerU()
+read_minerU = minerU_testing.read_minerU_output()
+format_minerU = minerU_testing.format_minerU_output()
 
+print(format_minerU)
 
-
-def format_minerU_output():
-
-        """
-        It takes the JSON output of the minerU, and fetches the information elements required to create the knowledge units. 
-
-        **Args:**
-        self.content_of_json (markdown): It is the parsed output in the JSON format.
-        ABS_PATH (Path): It is the absolute path that we need to use in order to convert the relative path of the table_image to the
-        absolute path. For now, we do not have the output of minerU path and our input path same (Because minerU is not running command statement correctly)
-        so, we are manually bringing the path of the minerU output manually. Otherwise, we would just have to import it using path.cwd().
-        
-        **Returns:**
-        self.content_list (list): It is the list of the knowledge units that contains the information about part of the document.
-
-        **Raises:**
-
-        """
-        # lets get the input of the function
-        minerU_raw_output = Parsed_minerU_raw
-        ABS_PATH = Path("C:\\Users\\Hp\\MinerU")
-
-
-        PDF_INFO = minerU_raw_output.get("pdf_info","")
-        content_list = []
-        table_images_folder = []
-
-
-        # Let's get the directory path of the output of the minerU so, that we can create the absolute path of the table images. 
-        for items in ABS_PATH.iterdir():
-            if items.is_dir():
-                table_images_folder.append(items)
-        ABS_PATH_OUTPUT = table_images_folder[1]
-        
-
-        # Lets get all the knowledge units in the content list
-        for page in PDF_INFO:
-            page_no = page.get("page_idx","")
-            discarded_block = page.get("discarded_blocks","")
-            para_blocks = page.get("para_blocks","")
-            for para_dict_details in para_blocks:
-                para_label = para_dict_details.get("type","")
-                if para_label not in ["table", "text", "title"]:
-                    continue
-                
-                # Knowledge units of textual content 
-                if para_label in ["text", "title"]:
-                    para_lines = para_dict_details.get("lines","")
-                    for lines_dict_details in para_lines:
-                        spans_details = lines_dict_details.get("spans","")
-                        for span_dict_details in spans_details:
-                                content_of_span = span_dict_details.get("content","")
-                                content_type_of_span = span_dict_details.get("type","")
-                                if content_type_of_span not in ["text"]:
-                                    continue
-                                knowledge_unit = {"page_no.":page_no, "raw_content":content_of_span, "content type": para_label}
-                                content_list.append(knowledge_unit)
-
-                # Knowledge units of tabular content
-                elif para_label == "table":
-                    table_blocks = para_dict_details.get("blocks","")
-                    for table_block in table_blocks:
-                        table_block_lines = table_block.get("lines","")
-                        for lines_dict_details in table_block_lines:
-                            spans_details = lines_dict_details.get("spans","")
-                            for table_span_dict_details in spans_details:
-                                    table_of_block_line = table_span_dict_details.get("image_path","")
-                                    content_type_of_span = table_span_dict_details.get("type","")
-                                    table_caption = table_span_dict_details.get("content","")
-                                    # Lets convert the relative path of the table image to the absolute path
-
-                                    ABS_table_image_path = ABS_PATH_OUTPUT.joinpath(f"images/{table_of_block_line}")
-                                    if content_type_of_span not in  ["table", "text"]:
-                                        continue
-                                    if content_type_of_span == "table":
-                                        knowledge_unit = {"page_no.":page_no, "table_image_path":ABS_table_image_path, "content_type": para_label}
-                                    if content_type_of_span == "text": 
-                                        knowledge_unit = {"page_no.":page_no, "table_caption":table_caption, "content_type": para_label}
-
-                                    content_list.append(knowledge_unit)
-                  
-
-
-        return content_list
-        
-
-content_list = format_minerU_output()
-print(content_list)
